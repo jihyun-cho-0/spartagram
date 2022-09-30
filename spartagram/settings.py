4898c61ea4fe9697cebd71ad98dd9937acba6ec0
didjsrne <hyunjae0723@gmail.com>
2022-09-30 11:40:22 +0900

    뼈대 생성

--- spartagram/settings.py
+++ spartagram/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-a*_35))x@ui+%)^h5ou(qmyfztggn3@_003a30@d9f9(#g0z_*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,10 +35,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'user', # 사용자 앱 추가
-    'tweet', # 게시글 앱 추가
+    'tweet',
+    'user',
+    'taggit.apps.TaggitAppConfig',
+    'taggit_templatetags2',
 ]
+
+TAGGIT_CASE_INSENSITIVE = True
+TAGGIT_LIMIT = 50
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'spartagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -72,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'spartagram.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -82,7 +83,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -102,7 +102,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -114,7 +113,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
@@ -124,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# spartagram/settings.py
+AUTH_USER_MODEL = 'user.UserModel'
--- tweet/views.py
+++ tweet/views.py
@@ -1,3 +1,91 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TweetModel, TweetComment
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, TemplateView
+
 
 # Create your views here.
+def home(request):
+    user = request.user.is_authenticated
+    if user:
+        return redirect('/tweet')
+    else:
+        return redirect('/sign-in')
+
+
+# tweet/views.py
+
+@login_required
+def tweet(request):
+    if request.method == 'GET':  # 요청하는 방식이 GET 방식인지 확인하기
+        all_tweet = TweetModel.objects.all().order_by('-created_at')
+        return render(request, 'tweet/home.html', {'tweet': all_tweet})
+    elif request.method == 'POST':  # 요청 방식이 POST 일때
+        user = request.user  # 현재 로그인 한 사용자를 불러오기
+        content = request.POST.get('my-content', '')  # 글 작성이 되지 않았다면 빈칸으로
+        tags = request.POST.get('tag', '').split(',')
+        if content == '':  # 글이 빈칸이면 기존 tweet과 에러를 같이 출력
+            all_tweet = TweetModel.objects.all().order_by('-created_at')
+            return render(request, 'tweet/home.html', {'error': '글은 공백일 수 없습니다', 'tweet': all_tweet})
+        else:
+            my_tweet = TweetModel.objects.create(author=user, content=content)  # 글 저장을 한번에!
+            for tag in tags:
+                tag = tag.strip()
+                if tag != '':  # 태그를 작성하지 않았을 경우에 저장하지 않기 위해서
+                    my_tweet.tags.add(tag)
+            my_tweet.save()
+            return redirect('/tweet')
+
+
+@login_required
+def delete_tweet(request, id):
+    my_tweet = TweetModel.objects.get(id=id)
+    my_tweet.delete()
+    return redirect('/tweet')
+
+
+@login_required
+def detail_tweet(request, id):
+    my_tweet = TweetModel.objects.get(id=id)
+    tweet_comment = TweetComment.objects.filter(tweet_id=id).order_by('-created_at')
+    return render(request, 'tweet/tweet_detail.html', {'tweet': my_tweet, 'comment': tweet_comment})
+
+
+@login_required
+def write_comment(request, id):
+    if request.method == 'POST':
+        comment = request.POST.get("comment", "")
+        current_tweet = TweetModel.objects.get(id=id)
+
+        TC = TweetComment()
+        TC.comment = comment
+        TC.author = request.user
+        TC.tweet = current_tweet
+        TC.save()
+
+        return redirect('/tweet/' + str(id))
+
+
+@login_required
+def delete_comment(request, id):
+    comment = TweetComment.objects.get(id=id)
+    current_tweet = comment.tweet.id
+    comment.delete()
+    return redirect('/tweet/' + str(current_tweet))
+
+
+class TagCloudTV(TemplateView):
+    template_name = 'taggit/tag_cloud_view.html'
+
+
+class TaggedObjectLV(ListView):
+    template_name = 'taggit/tag_with_post.html'
+    model = TweetModel
+
+    def get_queryset(self):
+        return TweetModel.objects.filter(tags__name=self.kwargs.get('tag'))
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tagname'] = self.kwargs['tag']
+        return context
