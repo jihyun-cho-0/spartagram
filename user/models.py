10fa2e1e1084ba9d9c03f71eb5e8389ad2e7fdb0
jihyun-cho-0 <roseview808@gmail.com>
2022-10-02 17:04:47 +0900

    수정 - 회원가입/로그인 기능
    1. 회원가입 페이지
    - bio 항목 삭제
    - email 항목 추가
    
    2. 수정  user/views.py
    - email 검증
    - email 검증 실패 시 오류 메시지 출

--- user/models.py
+++ user/models.py
@@ -1,4 +1,5 @@
 # user/models.py
+from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
@@ -8,5 +9,7 @@ class UserModel(AbstractUser):
     class Meta:
         db_table = "my_user"
 
-    bio = models.CharField(max_length=256, default='')
+    author_name = models.CharField(max_length=10, default='')
+    bio = models.CharField(max_length=100, default='')
     follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')
+    updated_at = models.DateTimeField(auto_now=True)
